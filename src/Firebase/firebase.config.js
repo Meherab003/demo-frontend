0ab3925a3c99c86e398d66bea3d54cf8d8acfdd4
Meherab Hosssain Bhuiyan <127032892+Meherab003@users.noreply.github.com>
2024-08-28 10:24:06 +0600

    demo

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,3 +1,25 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+//   appId: import.meta.env.VITE_APPID,
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export default app;
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,12 +27,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  apiKey: "AIzaSyC9d9KS2k4JXe45wxu4ht-gKBkshw7xCWc",
+  authDomain: "new-project-eb4cc.firebaseapp.com",
+  projectId: "new-project-eb4cc",
+  storageBucket: "new-project-eb4cc.appspot.com",
+  messagingSenderId: "413305744959",
+  appId: "1:413305744959:web:3493fea2ea22be6362528a"
 };
 
 // Initialize Firebase
